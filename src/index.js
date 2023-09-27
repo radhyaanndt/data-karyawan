6bd699c1ac5417238541b58ac7c11ebd7493d9d2
zidane arvito <zidanevito@gmail.com>
2023-09-27 22:31:10 +0700

    feat: add read excel file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const multer = require("multer");
+const excelToJson = require("convert-excel-to-json");
+const fs = require("fs-extra");
 const dotenv = require("dotenv");
 const router = require("./routes/router");
 
@@ -8,6 +11,34 @@ const app = express();
 
 app.use(express.json());
 app.use(router);
+
+var upload = multer({ dest: "uploads/" });
+
+app.post("/read", upload.single("file"), (req, res) => {
+  try {
+    if (req.file.filename == null || req.file?.filename == "undefined") {
+      res.status(400).json("no file uploaded");
+    } else {
+      var filePath = "uploads" + req.file.filename;
+
+      const excelData = excelToJson({
+        sourceFile: filePath,
+        header: {
+          rows: 1,
+        },
+        columnToKey: {
+          "*": "{{columnHeader}}",
+        },
+      });
+
+      fs.remove(filePath);
+
+      res.status(200).json(excelData);
+    }
+  } catch (error) {
+    res.status(500);
+  }
+});
 
 app.get("/", (req, res) => {
   return res.status(200).send({
