5d3d611f9a504b7682fee83d96549c77f416b265
zidane arvito <zidanevito@gmail.com>
2023-10-01 10:47:46 +0700

    improvement: add cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const fs = require("fs-extra");
 const dotenv = require("dotenv");
+const cors = require("cors")
 const router = require("./routes/router");
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(router);
 
