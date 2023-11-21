e589a3984ef7a4c25f87d92b2f67a2099a968d06
zidanearvito <zidanevito@gmail.com>
2023-11-21 16:49:29 +0700

    add delete data by timestamp

--- src/routes/dataKaryawan.js
+++ src/routes/dataKaryawan.js
@@ -2,6 +2,7 @@ const express = require("express");
 const multer = require("multer");
 const { authentication } = require("../middleware/authentication");
 const dataKaryawanController = require("../controllers/data-karyawan.controller");
+const employee_data = require("../models/employee_data");
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ const upload = multer({ dest: "./src/uploads/" }).single("file");
 router.get("/", dataKaryawanController.getData)
 router.post("/input-data", authentication, upload, dataKaryawanController.inputData);
 router.get("/total", dataKaryawanController.getTotal)
+router.delete("/delete", authentication, dataKaryawanController.deleteData)
 
 module.exports = router;
