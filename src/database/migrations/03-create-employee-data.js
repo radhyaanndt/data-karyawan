907f8babc1d61b3c0f80b083341f60e89f8ee9d1
zidanearvito <zidanevito@gmail.com>
2023-11-23 08:43:58 +0700

    fix: fix migration

--- src/database/migrations/03-create-employee-data.js
+++ src/database/migrations/03-create-employee-data.js
@@ -93,7 +93,7 @@ module.exports = {
         type: Sequelize.DATE,
       },
       deletedAt: {
-        allowNull: false,
+        allowNull: true,
         type: Sequelize.DATE,
       },
     });
