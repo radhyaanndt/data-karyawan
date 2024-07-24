9c14d0ba29786a7e19c640dc1e9da4d3cfe5c123
zidanearvito <zidanevito@gmail.com>
2024-07-24 19:33:30 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const router = require("./routes/router");
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3200;
 
 app.use(cors())
 app.use(express.json());
